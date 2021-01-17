9d2c2b50c1944df31a94f9e47cba46df506bfe7d
김진혁 <gandolkim@gmail.com>
2021-01-17 21:16:04 +0900

    spa made

--- spa1/src/index.js
+++ spa1/src/index.js
@@ -2,28 +2,44 @@ import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
+  const [pageName, setPageName] = useState('');
   useEffect(() => {
-    window.onpopstate = function(event) {
+    window.onpopstate = function (event) {
       console.log(`location: ${document.location}, state: ${event.state}`)
+      setPageName(event.state);
     };
-  }, [])
+  }, []);
+
+  function onClick1() {
+    const pageName = 'page1';
+    window.history.pushState(pageName, '', '/page1');
+    setPageName(pageName)
+  }
+
+  function onClick2() {
+    const pageName = 'page2';
+    window.history.pushState(pageName, '', '/page2');
+    setPageName(pageName)
+  }
 
   return (
     <div>
-      <button onClick={() => window.history.pushState('v1', '', '/page1')}>
+      <button onClick={onClick1}>
         page1
       </button>
-      <button onClick={() => window.history.pushState('v2', '', '/page2')}>
+      <button onClick={onClick2}>
         page2
       </button>
+      { !pageName && <Home /> }
+      { pageName === 'page1' && <Page1 /> }
+      { pageName === 'page2' && <Page2 />}
     </div>
   )
 }
 
-
 ReactDOM.render(
-<React.StrictMode>
-  <App />
-</React.StrictMode>,
-document.getElementById('root')
-)
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+) 
